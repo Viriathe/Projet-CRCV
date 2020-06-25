c3124e89a49fab78d8251966800d36ddefa79c14
Viriathe <51057912+Viriathe@users.noreply.github.com>
2020-06-25 17:51:57 +0200

    Add files via upload
    25.06

---
+++ slideMenu.js
@@ -0,0 +1,14 @@
+function openMenu(){
+  document.getElementById('slideMenu').style.marginLeft='0';
+}
+
+function closeMenu(){
+  document.getElementById('slideMenu').style.marginLeft='-290px';
+
+
+}
+
+function fullScreen(){
+  
+
+}
